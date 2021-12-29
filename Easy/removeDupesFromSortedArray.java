511b1b42e2980946cbdac1e6b8eb7e32819c76b5
Rossybossy235 <parkerhomeross@gmail.com>
2021-12-29 15:40:34 -0600

    remove duplicates from sorted array java solution

---
+++ Easy/removeDupesFromSortedArray.java
@@ -0,0 +1,32 @@
+package Easy;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int length = nums.length;
+        int solutionSize = nums.length;
+        int removed;
+        
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = i + 1; j < solutionSize; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    removed = nums[j];
+                    
+                    for (int k = j; k < length - 1; k++)
+                    {
+                        nums[k] = nums[k + 1];
+                    }
+                    
+                    nums[length - 1] = removed;
+                    solutionSize--;
+                    j--;
+                }
+            }
+        }
+        
+        return solutionSize;
+    }
+}
