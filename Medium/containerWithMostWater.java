f73936a21e8d4b2b451a3f2066020ad0932ec63a
Rossybossy235 <parkerhomeross@gmail.com>
2022-01-14 19:58:05 -0600

    container with most water java solution

---
+++ Medium/containerWithMostWater.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+        
+        int start = 0;
+        int end = height.length - 1;
+        int max = 0;
+        
+        while (start < end)
+        {
+            max = Math.max(max, (end - start) * Math.min(height[end], height[start]));
+            
+            if (height[end] > height[start])
+            {
+                start++;
+            }
+            else
+            {
+                end--;
+            }
+        }
+        
+        return max;
+    }
+}
