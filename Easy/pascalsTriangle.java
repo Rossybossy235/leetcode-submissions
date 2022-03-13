f549b4cff6e71b82fd608d7abdd12245f6e5c5fb
Rossybossy235 <parkerhomeross@gmail.com>
2022-03-12 20:00:37 -0600

    pascal's triangle java solution

---
+++ Easy/pascalsTriangle.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for (int i = 0; i < numRows; i++)
+        {
+            List<Integer> row = new ArrayList<Integer>();
+            
+            for (int j = 0; j < i + 1; j++)
+            {
+                if (j != 0 && j != i)
+                {
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+                else
+                {
+                    row.add(1);
+                }
+            }
+            
+            result.add(row);
+        }
+        
+        return result;
+    }
+}
