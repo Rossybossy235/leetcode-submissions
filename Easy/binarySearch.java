1c3391991f2011c0dddd2c9a21a2d2057e6041d7
Rossybossy235 <parkerhomeross@gmail.com>
2022-06-07 15:45:03 -0500

    binary search java solution

---
+++ Easy/binarySearch.java
@@ -0,0 +1,28 @@
+/*
+ * Binary Search - use the binary search algorithm to search for target value in an array
+ * 
+ * Use the binary search algorithm, good to memorize.
+ */
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        
+        while (hi >= lo) {
+            int mid = hi - lo / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                hi = mid - 1;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
