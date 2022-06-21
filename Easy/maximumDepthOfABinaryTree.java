250a6e7a83f7a3fddde174bc55455d04980e289a
Rossybossy235 <parkerhomeross@gmail.com>
2022-06-21 13:29:36 -0500

    max depth of binary tree description

--- Easy/maximumDepthOfABinaryTree.java
+++ Easy/maximumDepthOfABinaryTree.java
@@ -1,3 +1,11 @@
+/*
+ * Maximum Depth of a Binary Tree - Given the root node of a binary tree, determine the maximum
+ * depth of the tree.
+ * 
+ * We use a breadth-first search. At each step, we recursively call the left and right node of the
+ * current node. Then, we return the max of the left and right depths, plus one for the current level.
+ */
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
